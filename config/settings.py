0ba1d64fd7ceac0c1fa03aef78f8af74506a079e
svnviet <svnviet@gmail.com>
2021-05-23 15:42:53 +0700

    update

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news', #add
+    'movie',
+    'embed_video',
+    'movieapp',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +123,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ movie/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name= 'movie'
+urlpatterns = [
+    path('', HomeView, name='home'),
+    path('booksite/', booksite, name='booksite'),
+    path('movie-detail-<int:id>/', MovieDetail, name='MovieDetail')
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
